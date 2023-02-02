b582bfcfc5214b5cee98b1979e94d2252a50db24
Kyle Kryst <119367684+KyleKryst@users.noreply.github.com>
2023-02-02 13:30:30 -0800

    Started search-results.js
    Started trying to figure out some of the JavaScript for this page.

--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -1,1 +1,11 @@
+var resultTextEl = document.querySelector('#result-text');
+var resultContentEl = document.querySelector('#result-content');
+var searchFormEl = document.querySelector('#search-form');
 
+function getParams() {
+    var searchParamsArr = document.location.search.split('&');
+    var query = searchParamsArr[0].split('=').pop();
+    var format = searchParamsArr[1].split('=').pop();
+  
+    searchApi(query, format);
+  }
