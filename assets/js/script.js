b719cb53d3a02932bee1f4319af0b96fcb986bce
Kyle Kryst <119367684+KyleKryst@users.noreply.github.com>
2023-02-02 13:29:09 -0800

    Started script.js
    Started trying to figure out the JavaScript for this page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+  
+    var searchInputVal = document.querySelector('#search-input').value;
+  
+    if (!searchInputVal) {
+      console.error('You need a search input value!');
+      return;
+    }
+  
+    var queryString = './search-results.html?q=' + searchInputVal;
+  
+    location.assign(queryString);
+  }
+  
+  searchFormEl.addEventListener('submit', handleSearchFormSubmit);
